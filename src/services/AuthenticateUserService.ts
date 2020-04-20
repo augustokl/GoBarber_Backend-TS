51ae766db3f50e2f16bb518c42f5e6dee66f3bcf
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-04-19 23:22:23 -0300

    add global exeption handling

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,9 +1,11 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import User from '../models/User';
+
+import AppError from '../errors/AppError';
 
 import authConfig from '../config/auth';
-import User from '../models/User';
 
 interface Request {
   email: string;
@@ -24,13 +26,13 @@ class AuthenticaUserService {
     });
 
     if (!user) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
