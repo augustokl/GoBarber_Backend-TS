61fe93545d0809d8c979c7322a00cb9b9d82ddaf
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-05-29 22:26:58 -0300

    list providers

---
+++ src/modules/appoitments/infra/http/routes/providers.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import ProvidersController from '../controllers/ProvidersController';
+
+const appointmentsRouter = Router();
+const providersController = new ProvidersController();
+
+appointmentsRouter.use(ensureAuthenticated);
+
+// appointmentsRouter.get('/', async (request, response) => {
+//   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+//   const appointments = await appointmentsRepository.find();
+
+//   return response.json(appointments);
+// });
+
+appointmentsRouter.post('/', providersController.index);
+
+export default appointmentsRouter;
---
+++ src/modules/appoitments/services/ListProvidersService.spec.ts
@@ -0,0 +1,36 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProviders: ListProvidersService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  });
+  it('should be able to show list the providers', async () => {
+    const user1 = await fakeUsersRepository.create({
+      email: 'johndoe@test.com',
+      name: 'John Doe',
+      password: '123456',
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      email: 'johndoel@test.com',
+      name: 'John Doel',
+      password: '123456',
+    });
+
+    const loggedUser = await fakeUsersRepository.create({
+      email: 'john@test.com',
+      name: 'John Teste',
+      password: '123456',
+    });
+
+    const providers = await listProviders.execute({ user_id: loggedUser.id });
+
+    expect(providers).toEqual([user1, user2]);
+  });
+});
