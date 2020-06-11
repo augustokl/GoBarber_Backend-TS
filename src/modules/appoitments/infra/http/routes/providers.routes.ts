12e5f76a984d11389af1cc48c03e228f3273e41d
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-06-10 22:52:36 -0300

    add validation to routes

--- src/modules/appoitments/infra/http/routes/providers.routes.ts
+++ src/modules/appoitments/infra/http/routes/providers.routes.ts
@@ -1,3 +1,4 @@
+import { celebrate, Segments, Joi } from 'celebrate';
 import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
@@ -15,14 +16,29 @@ appointmentsRouter.use(ensureAuthenticated);
 appointmentsRouter.get('/', providersController.index);
 appointmentsRouter.get(
   '/:provider_id/month-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
   providerMonthAvailabilityController.index,
 );
 appointmentsRouter.get(
   '/:provider_id/day-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
   providerDayAvailabilityController.index,
 );
 appointmentsRouter.get(
   '/:provider_id/day-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
   providerDayAvailabilityController.index,
 );
 
