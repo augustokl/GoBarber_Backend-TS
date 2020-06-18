fb0db984dc410a452b38178da1dbe603dd7275d7
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-06-17 22:11:22 -0300

    some corretions

--- src/modules/appoitments/services/ListProviderAppointmentsService.ts
+++ src/modules/appoitments/services/ListProviderAppointmentsService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 import IAppoitmentsRepository from '@modules/appoitments/repositories/IAppoitmentsRepository';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
+import { classToClass } from 'class-transformer';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 interface IRequest {
@@ -42,7 +43,7 @@ class ListProviderAppointmentsService {
         year,
       });
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
