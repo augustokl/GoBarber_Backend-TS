1c0df993efdb50ff1dcc97abb594fc8c04ae4ff4
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-06-18 23:28:48 -0300

    add cors and modify reset password url

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import 'dotenv/config';
+import cors from 'cors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
@@ -15,6 +16,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(cors());
 app.use(rateLimiter);
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadFolder));
